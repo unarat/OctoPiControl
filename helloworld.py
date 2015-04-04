0aaef603cad7257af1f6870a61da172de77816b7
yk <yk>
2015-04-04 18:18:18 +0100

    Get IP address

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+import socket
+
+IP_Address = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+
+print(IP_Address)
+
